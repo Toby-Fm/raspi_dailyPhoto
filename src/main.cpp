9895593a369319737a91ae1b124e36d0eb4a6d6f
Toby-Fm <mail@toby-wichmann.de>
2024-04-29 20:06:50 +0200

    Greife auf Serial port zu und öffne die Webcam inkl bild in einem seperaten fenster

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,33 @@
+#include <opencv2/opencv.hpp>
 #include <iostream>
 
+using namespace cv;
+using namespace std;
+
 int main() {
-	std::cout << "Hello, World!" << std::endl;
+	VideoCapture cap(0); //Öffne die erste angeschlossene Kamera
+
+	if (!cap.isOpened()) {
+		cerr << "Kamera konnte nicht gefunden werden!" << endl;
+		return -1;
+	}
+
+	Mat frame;
+	while (true) {
+		cap >> frame; // Lese neues Frame von der Kamera
+		
+		if (frame.empty()) {
+			cerr << "Leeres Frame erhalten" << endl;
+			break;
+		}
+
+		imshow("Webcam", frame); // zeige das Frame in einem Fenster an
+		
+		if (waitKey(1) == 27) break;
+	
+	}
+
+	cap.release();
+	destroyAllWindows();
 	return 0;
 }
