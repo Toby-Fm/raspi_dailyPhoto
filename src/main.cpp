ffa1703a4841ae0004bb56512326c654f735f98e
Toby-Fm <mail@toby-wichmann.de>
2024-04-29 23:12:41 +0200

    Countdown für Foto, speicherung in extra img ordner und änderung von filename von Datum und Uhrzeit+

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
+#include <chrono>
+#include <thread>
+#include <iomanip>
+#include <sstream>
+#include <filesystem>
 
 using namespace cv;
 using namespace std;
+namespace fs = std::filesystem;
 
 int main() {
 	VideoCapture cap(0); //Öffne die erste angeschlossene Kamera
@@ -13,22 +19,42 @@ int main() {
 	}
 
 	Mat frame;
-	cap >> frame; // Lese Frame von der Kamera
 	
+	// Countdown für Foto-geschoss
+	cout << "Foto wird gemacht in: \n" << endl;
+	for (int i = 5; i > 0; --i) {
+		cout << i << endl;
+		this_thread::sleep_for (chrono::seconds(1));
+	}
+	cout << "\nFoto wird gemacht\n" << endl;
+
+	cap >> frame; // Lese Frame von der Kamera
 	if (frame.empty()) {
 		cerr << "Leeres Frame erhalten" << endl;
 		return -1;
 	}
 	
+	//Erstelle den Pfad, wenn er nicht existiert
+	string foldername = "img";
+	fs::create_directories(foldername);
+
+	// Hole das aktuelle Datum und die Uhrzeit
+	auto now = chrono::system_clock::now();
+	auto in_time_t = chrono::system_clock::to_time_t(now);
+
+	// Formatierujng von Datum und Uhrzeit für den Datei namen
+	stringstream ss;
+	ss << put_time(localtime(&in_time_t), "%d.%m.%Y_%H.%M");
+	string fileName = foldername + "/" + ss.str() + ".png";
+
 	//Speicher das Bild als PNG
-	bool isSaved = imwrite("nice.png", frame);
+	bool isSaved = imwrite(fileName, frame);
 	if(!isSaved) {
 		cerr << "Fehler beim speichern des Bildes!" << endl;
 		return -1;
 	}
 	
 	cout << "Bild erfolgreich gespeichert" << endl;
-
 	cap.release();
 	return 0;
 }
