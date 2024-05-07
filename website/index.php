5ddef1702a11c06e070de12f371e6c5f7b344e33
Toby-Fm <mail@toby-wichmann.de>
2024-05-07 23:56:53 +0000

    add index.php

--- website/index.php
+++ website/index.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body style="background: black; color: white;">
-    <h1>Hallo</h1>
-    <h2>Deine mutter</h2>
-    <h3>Deine Mutte 2</h3>
-</body>
+<html lang="de">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" href="./src/scss/index.css" >
+    </head>
+    <body>
+        <h1>Hallo</h1>
+        <h2>Deine Mutter 1</h2>
+        <h3>Deine Mutter 2</h3>
+    </body>
 </html>
