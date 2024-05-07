a8f1716cd1278b2278091b92d4464927ce095224
Toby-Fm <mail@toby-wichmann.de>
2024-05-07 23:06:12 +0000

    wo isn ditte

--- website/index.php
+++ website/index.php
@@ -5,9 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
+<body style="background: black; color: white;">
     <h1>Hallo</h1>
     <h2>Deine mutter</h2>
-
+    <h3>Deine Mutte 2</h3>
 </body>
 </html>
