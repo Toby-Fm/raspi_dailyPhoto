51d9dc4c5875d1624affe79d3dd7196d75395f17
Toby-Fm <mail@toby-wichmann.de>
2024-05-08 21:56:04 +0000

    open image in same window, and close

---
+++ website/src/js/index.js
@@ -0,0 +1,25 @@
+const openButtons = document.getElementsByClassName("open");
+const modal = document.getElementById("myModal");
+const modalImg = document.getElementById("img01");
+const captionText = document.getElementById("caption");
+const closeModal = document.getElementsByClassName("close")[0];
+
+for (let i = 0; i < openButtons.length; i++) {
+	openButtons[i].addEventListener("click", function () {
+		let imageBox = this.parentNode.parentNode;
+		let image = imageBox.querySelector("img");
+		modal.style.display = "block";
+		modalImg.src = image.src;
+		captionText.innerHTML = image.alt;
+	});
+}
+
+closeModal.addEventListener("click", function () {
+	modal.style.display = "none";
+});
+
+document.addEventListener("click", function (event) {
+	if (event.target === modal) {
+		modal.style.display = "none";
+	}
+});
