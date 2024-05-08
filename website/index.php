d9d52bf9c0231f7d3698dfa0d86b5426fe737201
Toby-Fm <mail@toby-wichmann.de>
2024-05-08 15:34:11 +0000

    erstelle eine mysql abfrage und gebe die sachen auf der seite aus

--- website/index.php
+++ website/index.php
@@ -8,3 +8,34 @@ if ($conn->connect_error) {
 }
 echo "Verbindung Erfolgreich<br><br>";
 ?>
+
+<!DOCTYPE html>
+<html lang="de">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Pflanzen</title>
+        <link rel="stylesheet" href="./src/scss/index.css" >
+    </head>
+    <body>
+        <?php 
+        echo $server, "<br>", $user, "<br>", $database, "<br><br>";
+        echo "<h1>Willkommen<br></h1>";
+
+        $erg = $conn->query("SELECT id, bild, bildname FROM bilder");
+        if ($erg === false) {
+            echo "Fehler: " . $conn->error;
+        } else {
+            while($row = $erg->fetch_assoc()) {
+                echo "ID: " . $row['id'] . "<br>";
+                if ($row['bild']) {
+                    // Die Bilddaten werden in Base64 umgewandelt und direkt im <img>-Tag eingefügt
+                    $base64 = 'data:image/png;base64,' . base64_encode($row['bild']);
+                    echo "<img src='" . $base64 . "' alt='" . htmlspecialchars($row['bildname']) . "'><br><br>";
+                }
+                echo "Bildname: " . $row['bildname'] . "<br><br>";
+            }
+        }
+        ?>
+    </body>
+</html>
