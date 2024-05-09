dd9cabfcb7cc311edacde769e82b521afccfadc2
Toby-Fm <mail@toby-wichmann.de>
2024-05-09 19:14:10 +0200

    db config template

---
+++ raspi/src/cpp/db.config.hpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <mysql/mysql.h>
+
+const char* server = "IP-Adresse";
+const char* user = "username";
+const char* password = "password";
+const char* database = "database";
