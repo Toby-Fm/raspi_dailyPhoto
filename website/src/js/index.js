c498b1a7fbea825f10d00f0bcabaa49f6d618afc
Toby-Fm <mail@toby-wichmann.de>
2024-05-09 01:00:31 +0000

    smooth open and close

--- website/src/js/index.js
+++ website/src/js/index.js
@@ -7,22 +7,27 @@ const closeModal = document.getElementsByClassName("close")[0];
 // Fügt jedem "open-button" einen EventListener hinzu
 for (let i = 0; i < openButtons.length; i++) {
 	openButtons[i].addEventListener("click", function () {
-		// holt sich das Bild und den Bildnamen aus dem übergeordneten Element
 		let imageBox = this.parentNode.parentNode;
 		let image = imageBox.querySelector("img");
-		modal.style.display = "block";
+		modal.style.display = "flex"; // Stellt das Modal auf flex, sodass es teil des DOM wird und transformierbar ist
+		setTimeout(() => {
+			modal.classList.add("show"); // Fügt die Klasse 'show' hinzu für die Animation
+		}, 1); // Kurze Verzögerung, um CSS-Transitions zu ermöglichen
 		modalImg.src = image.src;
 		captionText.innerHTML = image.alt;
 	});
 }
 
-// schließen
+// Schließen des Modals mit dem 'close' Button
 closeModal.addEventListener("click", function () {
-	modal.style.display = "none";
+	modal.classList.remove("show");
+	setTimeout(() => (modal.style.display = "none"), 500); // Stellt sicher, dass das Modal erst verschwindet, wenn die Animation abgeschlossen ist
 });
 
+// Schließen des Modals, wenn außerhalb des Inhalts geklickt wird
 document.addEventListener("click", function (event) {
 	if (event.target === modal) {
-		modal.style.display = "none";
+		modal.classList.remove("show");
+		setTimeout(() => (modal.style.display = "none"), 500); // Wartet bis die Schließanimation fertig ist
 	}
 });
